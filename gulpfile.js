36d3f32e9fd90b6c6329b2e2acd941f48b0bdf43
haowen <1057467537@qq.com>
2020-07-01 22:56:19 +0800

    '作业part2-1'

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+// 实现这个项目的构建任务
+const { src, dest, parallel, series, watch } = require('gulp')
+const del = require('del')
+const browserSync = require('browser-sync')
+const loadPlugins = require('gulp-load-plugins')
+
+const plugins = loadPlugins()
+const bs = browserSync.create()
+
+const clean = () => {
+  return del(['dist', temp])
+}
+
+const style = () => {
+  return src('src/assets/styles/*.scss', { base: 'src' })
+    .pipe(plugins.sass({ outputStyle: 'expanded' }))
+    .pipe(dest('dist'))
+    .pipe(bs.reload({ stream: true }))
+}
+
+const script = () => {
+  return src('src/assets/scripts/*.js', { base: 'src' })
+    .pipe(plugins.babel({ presets: ['@babel/preset-env'] }))
+    .pipe(dest('dist'))
+    .pipe(bs.reload({ stream: true }))
+}
+
+
+const page = () => {
+  return src('src/*.html', { base: 'src' })
+    .pipe(plugins.swig({ data, defaults: { cache: false } })) // 防止模板缓存导致页面不能及时更新
+    .pipe(dest('temp'))
+    .pipe(bs.reload({ stream: true }))
+}
+
+const image = () => {
+  return src('src/assets/images/**', { base: 'src' })
+    .pipe(plugins.imagemin())
+    .pipe(dest('dist'))
+}
+
+const font = () => {
+  return src('src/assets/fonts/**', { base: 'src' })
+    .pipe(plugins.imagemin())
+    .pipe(dest('dist'))
+}
+
+const extra = () => {
+  return src('public/**', { base: 'public' })
+    .pipe(dest('dist'))
+}
+
+const serve = () => {
+  watch('src/assets/styles/*.scss', style)
+  watch('src/assets/scripts/*.js', script)
+  watch('src/*.html', page)
+  // watch('src/assets/images/**', image)
+  // watch('src/assets/fonts/**', font)
+  // watch('public/**', extra)
+  watch([
+    'src/assets/images/**',
+    'src/assets/fonts/**',
+    'public/**'
+  ], bs.reload)
+
+  bs.init({
+    notify: false,
+    port: 2080,
+    // open: false,
+    server: {
+      baseDir: ['temp', 'src', 'public'],
+      routes: {
+        '/node_modules': 'node_modules'
+      }
+    }
+  })
+}
+
+module.exports = {
+  clean,
+  style,
+  script
+}
